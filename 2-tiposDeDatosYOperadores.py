36a9fba644801dd31e033591550dd2bfc3487e71
dimas-avila <67052002+dimas-avila@users.noreply.github.com>
2021-03-30 21:47:12 +0200

    Add files via upload

---
+++ 2-tiposDeDatosYOperadores.py
@@ -0,0 +1,56 @@
+#   Capítulo 2: Variables, Tipos de Datos y Operadores Aritméticos.
+
+#   Variables
+#   Tipos de datos numéricos
+
+numero_entero = 1
+print(numero_entero)
+
+numero_decimal = 2.46
+print(numero_decimal)
+
+numero_entero = numero_entero + 2
+print(numero_entero)
+
+numero = numero_entero + numero_decimal
+print(numero)
+
+numero_entero += 2
+print(numero_entero)
+
+variable_numerica = 20
+variable_numerica = variable_numerica - 10
+print(variable_numerica)
+
+variable = 10
+variable = variable / variable_numerica
+print(variable)
+
+numero = 2/2+3
+print(numero)
+
+multiplicacion = 2*3
+print(multiplicacion)
+multiplicacion = numero_entero*numero_decimal
+print(multiplicacion)
+
+potencia = 2**3
+modulo = 11%2
+print(potencia)
+print(modulo)
+
+#   Tipos de Datos: Texto
+texto = "Esta es nuestra frase"
+text = "Texto 'entre comillas' guay"
+textt = 'Texto "entre comillas" guay'
+txt = 'Hola'
+
+print(texto)
+print(text)
+print(textt)
+print(txt)
+
+texto_con_formato = """ Esto es un texto
+    con formato
+"""
+print(texto_con_formato)
